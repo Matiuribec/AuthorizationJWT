63398e89c75e53d175d4fd9f1ce6f01c5d819aa4
Matiuribec <matiuribec2020@gmail.com>
2023-02-05 23:44:38 +0000

    jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,59 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, create_refresh_token,jwt_required, get_jwt_identity, get_jwt
+from flask_bcrypt import Bcrypt
 
 api = Blueprint('api', __name__)
 
+cripto=Bcrypt(Flask(__name__))
+
+@api.route('/login', methods=['POST'])
+def user_login():
+
+    email=request.json.get('email')
+    password=request.json.get('password')
+    
+    user=User.query.filter(User.email==email).first()
+    if cripto.check_password_hash(user.password, password):
+        print("Clave correcta")
+        access_token=create_access_token(identity=user.id)
+        refresh_token=create_refresh_token(identity=user.id)
+        return jsonify({"msg": "Welcome Back!", "token":access_token,"refresh":refresh_token}), 200
+    else:
+    #clave no valilda
+        print("clave Invalida")
+        return jsonify({"msg":"Invalid Login"}), 401
+
+
+
+@api.route('/user_info', methods=['GET'])
+@jwt_required()
+def get_user_info():
+    user_id=get_jwt_identity()
+    user=User.query.get(user_id)
+    return jsonify(user.serialize())
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    email=request.json.get('email')
+    password=request.json.get('password')
+    user=User(email=email, password=cripto.generate_password_hash(password).decode('utf-8'),is_active=True)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"msg":"Usuario Creado"}),201
+   
+
+@api.route('/updatepassword', methods=['PATCH'])
+@jwt_required()
+def update_password():
+    new_password= request.json.get("password")
+    user_id=get_jwt_identity()
+    user=User.query.get(user_id)
+    user.password= cripto.generate_password_hash(new_password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"msg":"Password Changed"}), 200
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
